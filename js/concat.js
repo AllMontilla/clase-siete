d2ccdfd98d96230d058b97131f1c81f812a0b049
AllMontilla <allmontilla86@gmail.com>
2023-05-08 21:54:34 -0500

    practice-js

---
+++ js/concat.js
@@ -0,0 +1,20 @@
+let salute = "Hola, que tal";
+let pregunta = "¿Qué tal el día de hoy?";
+let alias = "Pancho";
+
+let concatenar = salute + " " + pregunta + " " + alias;
+console.log(concatenar);
+
+let saludo = "Hola Código LATAM";
+let question = "¿Qué Hay de Nuevo?";
+let saludito = "Saludos a todos";
+let despedida = "Hasta la próxima";
+
+let concate = saludo + " " + question + " " + saludito + " " + despedida;
+console.log(concate); 
+
+let saludin = "Saludos a todos";
+let frase = "Ya veremos la clase 8 en Código Pa' Lante, para aprender más sobre JS";
+
+let conca = saludin + " " + frase;
+console.log(conca);
---
+++ js/main.js
@@ -0,0 +1,36 @@
+let number_1 = 25;
+let number_2 = 40;
+let number_3 = 4;
+
+let suma1 = number_1 + number_2;
+console.log(suma1);
+
+let suma2 = number_1 + number_2 + number_3;
+console.log(suma2);
+
+let resta1 = number_1 - number_2;
+console.log(resta1);
+
+let resta2 = number_2 - number_3;
+console.log(resta2);
+
+let resta3 = number_1 - number_3;
+console.log(resta3);
+
+let multiplicacion = number_1 * number_2;
+console.log(multiplicacion);
+
+let multiplicar = number_1 * number_3;
+console.log(multiplicar);
+
+let division = number_1 / number_2;
+console.log(division);
+
+let dividir = number_2 / number_3;
+console.log(dividir);
+
+let resto = number_1 % number_2;
+console.log(resto);
+
+let rest = number_2 % number_3;
+console.log(rest);
